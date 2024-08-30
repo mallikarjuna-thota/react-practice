93f3a19a6434934bee2af5796f2d699a65d8f9f3
Mallikarjuna <malli7996@gamil.com>
2024-08-30 12:17:04 +0000

    Initial commit

--- app.jsx
+++ app.jsx
@@ -1,7 +1,46 @@
-export default function App() {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './Home';
+import About from './About';
+// ... import other pages
+
+function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            {/* ... other links */}
+          </ul>
+        </nav>
+
+        {/* A <Routes> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          {/* ... other routes */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
+export default App;
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<h1>Hello, world!</h1>);
+
+
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Hello World</h1>
+//     </div>
+//   );
+// }
